a5d3d6fdddb68493ce0594f2f96fc53fc5302300
Muhammad Talha Iqbal <93842094+talha-070@users.noreply.github.com>
2023-03-26 22:04:59 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:calculator/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
